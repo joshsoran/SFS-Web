fff5b584157a5645b09531c1bd911bca612a29b8
Josh Soran <joshsoran@gmail.com>
2021-07-29 17:11:48 +0300

    Updated info from online version

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <h1>Hey!</h1>
   <h2 style="font-weight: 370; line-height: 100%;">1. Need to send your information?</h2>
   <p> Click 'Signup' <a href="employeeSignup.php" style="font-size: 25px; text-decoration: none; color: #2c83ca;">here</a>, or up at the top!</p>
-  <h2 style="font-weight: 370;">2. Returning back?</h2>
+  <h2 style="font-weight: 370;">2. Need to update your information?</h2>
   <p>Click 'Login' <a href="loginMaster.php" style="font-size: 25px; text-decoration: none; color: #2c83ca;">here</a>, or up at the top and go from there.</p>  
   <br>
   <h4><center>NOTE: For the best experience, please use this website on a Desktop or Laptop computer.</center></h4>
